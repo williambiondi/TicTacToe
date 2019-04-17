5b95df8e4287860d2e3ec05b1ff0dbdcd11c0fc4
William Biondi <40241502+williambiondi@users.noreply.github.com>
2019-04-17 19:00:51 +0200

    Stable version
    Added a Play again button to clear the board and allow to start another game
    Fixed bug of the label under the board showing the wrong winner and turn

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,51 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var playerTurn = 1
     var board = [0,0,0,0,0,0,0,0,0]
     var players = ["Cross", "Knot"]
     let winningCombo = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-    var turn = 0
+    var turn = 1
+    var winner = -1
+    var nextTurn = -1
     
     @IBOutlet weak var reset: UIButton!
     @IBOutlet weak var label: UILabel!
     @IBAction func fillCell(_ sender: AnyObject) {
         //con il tag del sender identifico la cella toccata
         if board[sender.tag - 1] == 0{
-            if playerTurn == 1{
+            if turn % 2 != 0{
                 sender.setImage(UIImage(named: "cross.png"), for: UIControl.State()) //assegno l'immagine al bottone
-                playerTurn = 2 //cambio turno
+                winner = 1
+                nextTurn = 1
+                board[sender.tag - 1] = 1
             }
             else{
                 sender.setImage(UIImage(named: "circle.png"), for: UIControl.State())
-                playerTurn = 1
+                winner = 2
+                nextTurn = 0
+                board[sender.tag - 1] = 2
                 }
-            board[sender.tag - 1] = playerTurn //assegno la cella al giocatore
-            turn += 1
-            label.text = "It's \(players[playerTurn - 1]) turn"
+             //assegno la cella al giocatore
         }
         else{
             label.text = "This cell is already taken, retry"
         }
+        turn += 1
+        label.text = "Turn \(turn) :It's \(players[nextTurn]) turn"
         for combo in winningCombo{
             if board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && board[combo[0]] != 0 && board[combo[1]] != 0 && board[combo[2]] != 0{
-                playerTurn -= 1 //siccome ho cambiato turno torno indietro
-                label.text = "\(players[playerTurn - 1]) has won the game!"
+                label.text = "\(players[winner - 1]) has won the game!"
+                reset.isHidden = false
+                break
             }
-            else if turn == 8{
+            else if turn == 10{
                 label.text = "It's a draw!"
+                reset.isHidden = false
             }
         }
+      
     }
     
     
     @IBAction func startNewGame(_ sender: Any) {
+        board = [0,0,0,0,0,0,0,0,0]
+        turn = 1
+        winner = -1
+        for cell in 1...9{
+            let cellButton = view.viewWithTag(cell) as! UIButton
+            cellButton.setImage(nil, for: UIControl.State())
+        }
+        label.text = "Turn:\(turn) It's\(players[winner + 1]) turn"
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        label.text = "It's \(players[playerTurn - 1]) turn"
+        label.text = "Turn:\(turn) It's \(players[winner + 1]) turn"
+        reset.isHidden = true
 
     }
 
